1b30d71a5cf1d4074e7975666713fa7f03eeefb3
batallan <batallanmaximo03@gmail.com>
2024-05-21 13:42:49 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -42,6 +42,62 @@ if(control)
 }else{
     Console.WriteLine("No es numero, ingrese nuevamente");
 }*/
+//op=int.Parse(Console.ReadLine()); //Entrada como cadena y Convierte la cadena en entero
 
 //EJERCICIO 2
 
+int op, resp, num1, num2;
+
+do
+{
+    Console.WriteLine("Operaciones:");
+    Console.WriteLine("1.Sumar");
+    Console.WriteLine("2.Restar");
+    Console.WriteLine("3.Multiplicar");
+    Console.WriteLine("4.Dividir");
+    Console.WriteLine("Selecionar Operacion: ");
+    bool controlOp=int.TryParse(Console.ReadLine(), out op);
+
+    if(controlOp)
+    {
+        //Controlo que cadena ingresada sea un numero
+        Console.WriteLine("Ingrese un numero: ");
+        bool control1=int.TryParse(Console.ReadLine(), out num1);
+
+        Console.WriteLine("Ingrese otro numero: ");
+        bool control2=int.TryParse(Console.ReadLine(), out num2);
+
+        if(control1 && control2)
+        {
+            switch (op)
+            {
+                case 1:
+                Console.WriteLine("El resultado de la Suma es:"+(num1+num2));
+                break;
+                case 2: 
+                Console.WriteLine("El resultado de la Diferencia es:"+(num1-num2));
+                break;
+                case 3:
+                Console.WriteLine("El resultado del Producto es:"+(num1*num2));
+                break;
+                case 4:
+                Console.WriteLine("El resultado de la Division es:"+(num1/num2));
+                break;
+
+                default:
+                Console.WriteLine("La Operacion Ingresada no existe");
+                break;
+            }
+
+        }else{
+            Console.WriteLine("Algun dato Ingresado no es un numero, Ingrese nuevamente");
+        }
+
+    }else{
+        Console.WriteLine("Operacion Ingresada Incorrecta");
+    }
+
+    Console.WriteLine("Desea Realizar otra Operacion?(Si=1)(No=0)");
+    bool controlResp=int.TryParse(Console.ReadLine(), out resp);
+    
+} while (resp!=0);
